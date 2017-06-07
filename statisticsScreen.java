b3af2db94a3b1f106b00329a44861af64c68cb5e
Michael Kim <MichaelKim3@hotmail.com>
2017-06-06 23:12:24 -0400

    6/5 - 6/6 Changelog
    - Addition of 4 specific statistics.
    - Addition of generic statistics.

--- statisticsScreen.java
+++ statisticsScreen.java
@@ -3,6 +3,7 @@ package michaelkim.budgetingandwalletbased;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +13,9 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class statisticsScreen extends Fragment {
 
@@ -20,6 +23,19 @@ public class statisticsScreen extends Fragment {
     ArrayList<String> categoryNames;
     ArrayList<Category> categories;
     ArrayList<Transaction> transactions;
+
+    // Decimal format for viewing values in proper monetary format.
+    DecimalFormat df = new DecimalFormat("#.00");
+
+    // Random statistics declaration.
+    TextView mss;
+    TextView fvs;
+    TextView ltas;
+    TextView lts;
+
+    // General statistics declaration.
+    HashMap<String, Double> hashMap = new HashMap<String, Double>();
+    ArrayList<Category> sampStats = new ArrayList<Category>();
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
@@ -60,14 +76,93 @@ public class statisticsScreen extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_statistics_screen, container, false);
 
+        // Populating all the values of the HashMap and ArrayList for the generic statistics.
+        for (int j = 0; j < transactions.size(); j ++){
+            if (hashMap.containsKey(transactions.get(j).location) && transactions.get(j).value.charAt(0) == '-'){
+                double currValue = hashMap.get(transactions.get(j).location);
+                hashMap.put(transactions.get(j).location, currValue + Double.parseDouble(transactions.get(j).value.substring(3)));
+                Log.e("VALUE", transactions.get(j).value);
+            }
+            else if (transactions.get(j).value.charAt(0) == '-' && !hashMap.containsKey(transactions.get(j).location)){
+                hashMap.put(transactions.get(j).location, Double.parseDouble(transactions.get(j).value.substring(3)));
+                Log.e("VALUE", transactions.get(j).value);
+            }
+        }
+
+        for (HashMap.Entry<String, Double> entry : hashMap.entrySet()) {
+            sampStats.add(new Category(entry.getKey(), entry.getValue()));
+        }
+
         // View and populate the list of statistics.
         ListView listOfStatistics = (ListView) view.findViewById(R.id.listOfStatistics);
         statisticsAdapter statsAdapter = new statisticsAdapter();
         listOfStatistics.setAdapter(statsAdapter);
 
+        // Provide values for random statistics for all categories.
+        // Total Money Spent Statistic.
+        double totalSpent = 0;
+        for (int i = 0; i < transactions.size(); i ++){
+            if (transactions.get(i).value.charAt(0) == '-') {
+                totalSpent = totalSpent + Double.parseDouble(transactions.get(i).value.substring(3));
+            }
+        }
+        mss = (TextView) view.findViewById(R.id.mSS);
+        mss.setText("$" + String.format("%.2f", totalSpent));;
+
+        // Most Visited Location Statistic.
+        String highestLocation = "";
+        int highestLocationValue = 0;
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        for (int i = 0; i < transactions.size(); i ++){
+            if (map.containsKey(transactions.get(i).location)){
+                int currValue = map.get(transactions.get(i).location);
+                map.put(transactions.get(i).location, currValue + 1);
+                if (map.get(transactions.get(i).location) > highestLocationValue){
+                    highestLocation = transactions.get(i).location;
+                    highestLocationValue = map.get(transactions.get(i).location);
+                }
+            }
+            else {
+                map.put(transactions.get(i).location, 1);
+                if (map.get(transactions.get(i).location) > highestLocationValue){
+                    highestLocation = transactions.get(i).location;
+                    highestLocationValue = map.get(transactions.get(i).location);
+                }
+            }
+        }
+        fvs = (TextView) view.findViewById(R.id.fVS);
+        fvs.setText(highestLocation + " visited " + highestLocationValue + " times.");
+
+        // Largest Sum Added Statistic.
+        double largestAdded = 0;
+        for (int i = 0; i < transactions.size(); i ++){
+            if (transactions.get(i).value.charAt(0) == '+') {
+                if (Double.parseDouble(transactions.get(i).value.substring(3)) > largestAdded) {
+                    largestAdded = Double.parseDouble(transactions.get(i).value.substring(3));
+                }
+            }
+        }
+        ltas = (TextView) view.findViewById(R.id.lTAS);
+        ltas.setText("+ $" + String.format("%.2f", largestAdded));;
+
+        // Largest Sum Subtracted Statistic.
+        double largestSubtracted = 0;
+        for (int i = 0; i < transactions.size(); i ++){
+            if (transactions.get(i).value.charAt(0) == '-') {
+                if (Double.parseDouble(transactions.get(i).value.substring(3)) > largestSubtracted) {
+                    largestSubtracted = Double.parseDouble(transactions.get(i).value.substring(3));
+                }
+            }
+        }
+        lts = (TextView) view.findViewById(R.id.lTS);
+        lts.setText("- $" + String.format("%.2f", largestSubtracted));
+
         // Instantiate the category spinner and populate it with its category names.
-        Spinner categorySpinner = (Spinner) view.findViewById(R.id.categorySpinner);
-        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, categoryNames);
+        final Spinner categorySpinner = (Spinner) view.findViewById(R.id.categorySpinner);
+        final ArrayList<String> withTotal = new ArrayList<String>();
+        withTotal.add("All Categories");
+        withTotal.addAll(categoryNames);
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, withTotal);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         categorySpinner.setAdapter(spinnerAdapter);
 
@@ -80,7 +175,7 @@ public class statisticsScreen extends Fragment {
 
         @Override
         public int getCount() {
-            return categoryNames.size();
+            return sampStats.size();
         }
 
         @Override
@@ -100,19 +195,12 @@ public class statisticsScreen extends Fragment {
             final TextView categoryName = (TextView) view.findViewById(R.id.moneySpentStats);
             final TextView categoryValue = (TextView) view.findViewById(R.id.$spentStats);
 
-            categoryName.setText("Money Spent in Category " + categories.get(i).name + " : ");
+            // Create a HashMap that stores all the values of :
+            //    The Locations
+            //    Total Amount of Money Spent at Each
 
-            double totalSpent = 0;
-
-
-            for (int j = 0; j < transactions.size(); j++){
-                if (transactions.get(j).name.equals(categories.get(i).name) && transactions.get(j).value.charAt(0) == '-'){
-                    double value = Double.parseDouble(transactions.get(j).value.substring(3));
-                    totalSpent = totalSpent + value;
-                }
-            }
-
-            categoryValue.setText("- $" + Double.toString(totalSpent));
+            categoryName.setText("Money spent at " + sampStats.get(i).name);
+            categoryValue.setText(String.format("%.2f", sampStats.get(i).value));
 
             return view;
         }
